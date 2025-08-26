399d51ca866f90b579eef7de31cfde265447a2ff
Auresol69 <quocbao962005@gmail.com>
2025-08-26 08:22:56 +0100

    Song sieu am SRF05

--- Song_sieu_am_SRF05/Song_sieu_am_SRF05.ino
+++ Song_sieu_am_SRF05/Song_sieu_am_SRF05.ino
@@ -1,21 +1,35 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 #include <NewPing.h>
 
-#define TRIGGER_PIN 2
-#define ECHO_PIN 3
-#define MAX_DISTANCE 400
+LiquidCrystal_I2C lcd(0x27,16,2);
+
+#define TRIGGER_PIN 11
+#define ECHO_PIN 12
+#define MAX_DISTANCE 200
 
 NewPing sonar(TRIGGER_PIN,ECHO_PIN,MAX_DISTANCE);
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  lcd.init();
+  lcd.backlight();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  delay(500);
+  delay(1000);
   unsigned int uS = sonar.ping();
   Serial.print("Distance: ");
   Serial.print(uS/US_ROUNDTRIP_CM);
   Serial.println("cm");
+  lcd.setCursor(0, 0);
+  lcd.print("Distance:");
+  lcd.setCursor(0, 1);
+  lcd.print("            ");
+  lcd.setCursor(9, 1);
+  lcd.print(uS/US_ROUNDTRIP_CM);
+  lcd.setCursor(12, 1);
+  lcd.print("cm");
 }
